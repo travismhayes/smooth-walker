5ee6597ac3ef4350d0535413906b300d6523da33
Travis <travis@travismhayes.com>
2022-05-31 11:33:09 -0700

    Initial Commit

---
+++ SmoothWalker/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  SmoothWalker
+//
+//  Created by travis hayes on 5/31/22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ SmoothWalker/SmoothWalkerApp.swift
@@ -0,0 +1,17 @@
+//
+//  SmoothWalkerApp.swift
+//  SmoothWalker
+//
+//  Created by travis hayes on 5/31/22.
+//
+
+import SwiftUI
+
+@main
+struct SmoothWalkerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
